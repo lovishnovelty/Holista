5c27a99afe6a81148c2679b87cf30e909a675eda
Suprim Shakya <suprim@noveltytechnology.com>
2023-08-21 17:18:26 +0545

    disclaimer text changes

--- src/common/ui/disclamier.tsx
+++ src/common/ui/disclamier.tsx
@@ -61,6 +61,14 @@ const Desclamier = (props: any) => {
               style={ds.text}
               title="The information, including but not limited to, text, graphics, images and other material contained on this website are for informational purposes only. No material on this site is intended to be a substitute for professional medical advice, diagnosis or treatment. Always seek the advice of your physician or other qualified health care provider with any questions you may have regarding a medical condition or treatment and before undertaking a new health care regimen, and never disregard professional medical advice or delay in seeking it because of something you have read on this website."
             />
+            <RegularText
+              style={ds.text}
+              title="A quote of benefits and/or authorization does not guarantee payment or verify eligibility. Payment of benefits are subject to all terms, conditions, limitations, and exclusions of the member’s contract at the time of service."
+            />
+            <RegularText
+              style={ds.text}
+              title="I understand that my health insurance company may deny or limit payment for the services identified, for any reason (stated or not). If my health insurance company denies payment, I agree to be personally and fully responsible for payment. I also understand that if my health insurance company does make payment for services, I will be responsible for any co-payment, deductible, or coinsurance that applies."
+            />
             <View style={ds.checkBox}>
               <CheckBox
                 style={{flex: 1, padding: normalize(10)}}
